013551b9e2cd70f5cc811ba0583f097cefced6b5
vinia <viniciusadams98@gmail.com>
2023-08-07 23:27:48 +0100

    starting new project

---
+++ src/main/Board.java
@@ -0,0 +1,11 @@
+package main;
+import javax.swing.*;
+import java.awt.*;
+public class Board {
+int cols = 8;
+int rows=8;
+    public Board(){
+
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setMinimumSize(new Dimension(1000, 1000)); // Fixed the typo here
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
