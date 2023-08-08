2cfe6347bbbc9cac3aa02ef0987b82e74c537c4d
vinia <viniciusadams98@gmail.com>
2023-08-08 23:50:41 +0100

    finishing the board setup and making it able to run

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,15 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setMinimumSize(new Dimension(1000, 1000)); // Fixed the typo here
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        Board board = new Board(); // Create an instance of the Board panel
+        frame.add(board); // Add the Board panel to the JFrame
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
     }
 }
