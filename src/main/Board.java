4b61426a4548efb515f1f735b1379d91839ff7c0
vinia <viniusadams98@gmail.com>
2023-08-17 18:21:09 +0100

    commiting changes made on classe and adding all the white pieces

--- src/main/Board.java
+++ src/main/Board.java
@@ -18,7 +18,7 @@ public class Board extends JPanel {
 //setting background to green and applying the dimensions
     }
     public void addPieces (){
-
+        //implementing all the black pieces
         pieceList.add (new Rook(this,0,0,false));
         pieceList.add (new Knight(this,1,0,false));
         pieceList.add (new Bishop(this,2,0,false));
@@ -37,7 +37,24 @@ public class Board extends JPanel {
         pieceList.add (new Pawn(this,6,1,false));
         pieceList.add (new Pawn(this,7,1,false));
 
-
+//implementing all the withe pieces
+        pieceList.add (new Rook(this,0,7,true));
+        pieceList.add (new Knight(this,1,7,true));
+        pieceList.add (new Bishop(this,2,7,true));
+        pieceList.add (new Queen(this,3,7,true));
+        pieceList.add (new King(this,4,7,true));
+        pieceList.add (new Bishop(this,5,7,true));
+        pieceList.add (new Knight(this,6,7,true));
+        pieceList.add (new Rook(this,7,7,true));
+//////////
+        pieceList.add (new Pawn(this,0,6,true));
+        pieceList.add (new Pawn(this,1,6,true));
+        pieceList.add (new Pawn(this,2,6,true));
+        pieceList.add (new Pawn(this,3,6,true));
+        pieceList.add (new Pawn(this,4,6,true));
+        pieceList.add (new Pawn(this,5,6,true));
+        pieceList.add (new Pawn(this,6,6,true));
+        pieceList.add (new Pawn(this,7,6,true));
 
 
 
