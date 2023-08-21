ff59427a75634a4df2838a41b8280ba1c2cc8eb4
vinia <viniusadams98@gmail.com>
2023-08-21 09:12:29 +0100

    completing the methods in input

--- src/main/Input.java
+++ src/main/Input.java
@@ -1,5 +1,7 @@
 package main;
 
+import pieces.Piece;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -14,21 +16,33 @@ public Input (Board board){
 
     @Override
     public void mousePressed(MouseEvent e) {
-    int col = e.getX() / board.tileSize;
-    int row = e.getX() / board.tileSize;
+        int col = e.getX() / board.tileSize;
+        int row = e.getX() / board.tileSize;
 
-
+        Piece pieceXY = board.getPiece(col, row);
+        if (pieceXY != null) {
+            board.selectedPiece = pieceXY;
+        }
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
+    if (board.selectedPiece != null){
+        board.selectedPiece.xPos = e.getX() - board.tileSize /2;
+        board.selectedPiece.xPos = e.getY() - board.tileSize /2;
+
+        board.repaint();
+    }
+
+
 
     }
 
 
     @Override
     public void mouseDragged(MouseEvent e) {
-
+    int col = e.getX() / board.tileSize;
+    int row = e.getY() / board.tileSize;
     }
 
 }
