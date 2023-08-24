b5884772132518177e144e7fbc0af957cbfcd82f
vinia <viniusadams98@gmail.com>
2023-08-24 22:37:37 +0100

    working a bit in board class

--- src/main/Board.java
+++ src/main/Board.java
@@ -108,19 +108,28 @@ public boolean sameTeam (Piece p1, Piece p2){
     public void paintComponent(Graphics g) {
 
         Graphics2D g2d = (Graphics2D) g;
-        for (int r = 0; r < rows; r++) {
+        for (int r = 0; r < rows; r++)
             for (int c = 0; c < cols; c++) {
                 g2d.setColor((c + r) % 2 == 0 ? new Color (227,198,181) : new Color (157 ,105,53));
                 g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
             }
-        for (Piece piece : pieceList){
+
+            for (int r=0; r < rows ; r++)
+                for (int c = 0; c < cols;c++){
+
+                if (isValidMove(new Move (this, selectedPiece,c,r))){
+                    g2d.setColor(new Color(68,100,5,190));
+                }
+            }
+
+            for (Piece piece : pieceList){
             piece.paint(g2d);
         }
         }
     }
 
 
-}
+
 
 
 
